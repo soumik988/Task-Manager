e7fef32e938b8a27c749d4d52e41257f0126edf5
iamsoumik <153822678+soumik988@users.noreply.github.com>
2026-03-22 07:31:39 +0530

    middleware to handle CORS

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,19 @@
 import express from "express"
+import cors from "cors"
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+
+dotenv.config()
+
 
 const app=express()
+app.use(cors({
+    origin:process.env.FRONT_END_URL || "http://localhost:5173",
+    methods:["GET","POST","DELETE","PUT"],
+    credentials:true,
+}))
+
+app.use(express.json())
 
 app.listen(3000,()=>{
     console.log("server is running on port 3000..")
