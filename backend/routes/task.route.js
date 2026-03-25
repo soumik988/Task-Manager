a94e09d3a5aae7b2fc3d46b4ab70bbeb4975d05c
iamsoumik <153822678+soumik988@users.noreply.github.com>
2026-03-25 16:36:54 +0530

    task route

--- backend/routes/task.route.js
+++ backend/routes/task.route.js
@@ -1,13 +1,19 @@
 import express from "express"
 import { adminOnly, verifyToken } from "../utils/verifyUser.js"
-import { createTask } from "../controller/task.controller.js"
+import { createTask, getTasks, getTasksById, updateTask } from "../controller/task.controller.js"
 
 
 const router=express.Router()
 
 router.post("/create",verifyToken,adminOnly,createTask)
 
-//router.get("/",verifyToken,getTasks)
+router.get("/",verifyToken,getTasks)
+
+
+router.get("/:id",verifyToken,getTasksById)
+
+
+router.put("/:id",verifyToken,updateTask )
 
 
 
